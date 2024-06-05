ac26098dda75069b547e855011b128b571a1d23c
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2024-06-05 13:23:09 -0400

    console log test

--- index.js
+++ index.js
@@ -59,81 +59,85 @@ function getGame(id) {
   return firestore.collection("games").doc(id).get();
 }
 app.post("/listen", async (req, res) => {
-  const body = req.body;
-  const apiKey = body.apiKey;
-  if (apiKey !== process.env.API_KEY) {
-    return res.status(401).send("Unauthorized");
-  }
-  const room = body.room;
-  if (!room) {
-    return res.status(400).send("Room is required");
-  }
-  if (!(await getGame(room)).exists) {
-    return res.status(400).send("Game does not exist");
-  }
-  const tutorialName = body.tutorialName;
-  const trackingId = body.trackingId;
-  if (!trackingId) {
-    return res.status(400).send("Tracking ID is required");
-  }
-  if (!room) {
-    return res.status(400).send("Room is required");
-  }
-  const roomData = roomsListening.find((r) => r.room === room);
-  if (roomData !== undefined) {
-    return res.status(200).send("Already listening");
-  }
-  let ydoc = new Y.Doc();
-  let provider = new WebrtcProvider(room, ydoc, {
-    signaling: ["wss://yjs-signaling-server-5fb6d64b3314.herokuapp.com"],
-  });
-
-  let code = ydoc.getText("codemirror").toString();
-  let firstUpdated = true;
-  ydoc.on("update", () => {
-    if (!firstUpdated) {
-      provider.awareness.setLocalState({ saved: "saving" });
-      return;
+  try {
+    const body = req.body;
+    const apiKey = body.apiKey;
+    if (apiKey !== process.env.API_KEY) {
+      return res.status(401).send("Unauthorized");
     }
-    firstUpdated = false;
-    setInterval(async () => {
-      if (provider.awareness.getStates().size <= 1) {
-        provider.awareness.setLocalStateField("saved", "error");
+    const room = body.room;
+    if (!room) {
+      return res.status(400).send("Room is required");
+    }
+    if (!(await getGame(room)).exists) {
+      return res.status(400).send("Game does not exist");
+    }
+    const tutorialName = body.tutorialName;
+    const trackingId = body.trackingId;
+    if (!trackingId) {
+      return res.status(400).send("Tracking ID is required");
+    }
+    if (!room) {
+      return res.status(400).send("Room is required");
+    }
+    const roomData = roomsListening.find((r) => r.room === room);
+    if (roomData !== undefined) {
+      return res.status(200).send("Already listening");
+    }
+    let ydoc = new Y.Doc();
+    let provider = new WebrtcProvider(room, ydoc, {
+      signaling: ["wss://yjs-signaling-server-5fb6d64b3314.herokuapp.com"],
+    });
+
+    let code = ydoc.getText("codemirror").toString();
+    let firstUpdated = true;
+    ydoc.on("update", () => {
+      if (!firstUpdated) {
+        provider.awareness.setLocalState({ saved: "saving" });
         return;
       }
-      const metricKey = "database.update";
-      code = ydoc.getText("codemirror").toString();
+      firstUpdated = false;
+      setInterval(async () => {
+        if (provider.awareness.getStates().size <= 1) {
+          provider.awareness.setLocalStateField("saved", "error");
+          return;
+        }
+        const metricKey = "database.update";
+        code = ydoc.getText("codemirror").toString();
 
-      await timedOperation(metricKey, async () => {
-        firestore
-          .collection("games")
-          .doc(room)
-          .update({
-            code,
-            modifiedAt: Timestamp.now(),
-            tutorialName: tutorialName ?? null,
-          });
-      });
+        await timedOperation(metricKey, async () => {
+          firestore
+            .collection("games")
+            .doc(room)
+            .update({
+              code,
+              modifiedAt: Timestamp.now(),
+              tutorialName: tutorialName ?? null,
+            });
+        });
 
-      await timedOperation(metricKey, async () => {
-        firestore
-          .collection("daily-edits")
-          .doc(`${room}-${new Date().toDateString()}`)
-          .set({
-            type: "game",
-            date: Timestamp.now(),
-            id: room,
-          });
-      });
-      provider.awareness.setLocalStateField("saved", "saved");
-    }, 2000);
-  });
-  roomsListening.push({
-    room: room,
-    provider: provider,
-    ydoc: ydoc,
-  });
-  res.status(200).send("Listening to room " + room);
+        await timedOperation(metricKey, async () => {
+          firestore
+            .collection("daily-edits")
+            .doc(`${room}-${new Date().toDateString()}`)
+            .set({
+              type: "game",
+              date: Timestamp.now(),
+              id: room,
+            });
+        });
+        provider.awareness.setLocalStateField("saved", "saved");
+      }, 2000);
+    });
+    roomsListening.push({
+      room: room,
+      provider: provider,
+      ydoc: ydoc,
+    });
+    res.status(200).send("Listening to room " + room);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.post("/stop", (req, res) => {
